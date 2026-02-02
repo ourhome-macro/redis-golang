3f28cb8ecd580917c4f24b511cca420fbd6c0cf2
Macro <2622123804@qq.com>
2026-02-03 00:16:43 +0800

    feat：提交echohandler的close方法以及预创resp协议解析器

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "MiddlewareSelf/tcp"
+import (
+	"MiddlewareSelf/tcp"
+	"log"
+	"time"
+)
 
 func main() {
-	tcp.ListenAndServe(":8080")
+	// 1. 准备配置
+	// 这里配置监听本地的 8080 端口，设置最大连接数和超时时间
+	cfg := &tcp.Config{
+		Address:    ":8080",
+		MaxConnect: 1000,
+		Timeout:    10 * time.Second,
+	}
+
+	// 2. 准备 Handler
+	// 使用你之前定义好的 EchoHandler
+	// 因为 EchoHandler 已经实现了 Handle 和 Close 方法，所以它实现了 Handler 接口
+	handler := tcp.MakeEchoHandler()
+
+	// 3. 启动服务
+	// 这个函数会阻塞在这里，直到收到退出信号（比如 Ctrl+C）或者发生严重错误
+	log.Println("Server is preparing to start...")
+	err := tcp.ListenAndServeWithSignal(cfg, handler)
+	if err != nil {
+		log.Fatalf("Server start failed: %v", err)
+	}
+
+	log.Println("Server exited gracefully")
 }
---
+++ redis/interface/reply.go
@@ -0,0 +1,5 @@
+package _interface
+
+type Reply interface {
+	toBytes() []byte
+}
